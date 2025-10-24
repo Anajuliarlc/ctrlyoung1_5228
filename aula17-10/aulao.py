27abe2dbc9de9d5acecb34b7f72a4b36bc935aa4
Ana Julia Lima <101828635+Anajuliarlc@users.noreply.github.com>
2025-10-24 13:22:39 -0300

    Update aulao.py

--- aula17-10/aulao.py
+++ aula17-10/aulao.py
@@ -1,1 +1,7 @@
-print("Hello, world!")
+def media(n1, n2):
+    media_final = (n1 + n2) / 2
+    return media_final
+
+a = float(input("Primeira nota: "))
+b = float(input("Segunda nota: "))
+print("A media é:", media(a, b))
